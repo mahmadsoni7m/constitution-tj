87f1cc4472cbbe69365b452934a7f9395db6d762
mahmadsoni7m <mahmadsoni7@gmail.com>
2026-09-20 10:07:43 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+kotlinOptions {
+    jvmTarget = "17"
+    allWarningsAsErrors = false
+}
 
     buildFeatures {
         compose = true
